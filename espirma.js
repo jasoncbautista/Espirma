d1535e386d986a2a92412284dd69491475224e6d
Jason <bjason@gmail.com>
2014-02-02 09:01:13 -0800

    Figured out comments.

--- espirma.js
+++ espirma.js
@@ -1,3 +1,3 @@
 var esprima = require('esprima');
 //console.log(JSON.stringify(esprima.parse('var answer = 42'), null, 4));
-console.log(JSON.stringify(esprima.parse('//Testing one two \n;var answer = 42'), null, 4));
+console.log(JSON.stringify(esprima.parse('//Testing one two \n;var answer = 42', {comment: true}), null, 4));
