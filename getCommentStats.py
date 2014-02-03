6ee98858ee356cbf5e829390ffbfa5e2dc739bf7
Jason <bjason@gmail.com>
2014-02-02 21:35:35 -0800

    Comment stats in json

--- getCommentStats.py
+++ getCommentStats.py
@@ -10,7 +10,9 @@ import os
 
 fileName = sys.argv[1]
 
-print fileName
+
+
+
 
 
 
@@ -18,6 +20,29 @@ print fileName
 
 loc = os.popen("./findLinesInFile.sh " + fileName).read()
 commentCount= os.popen("node getCommentCount.js " + fileName).read()
+actualCodeCount =  int(loc) - int(commentCount)
 
+
+commentDesnity=    float(commentCount) / float(loc)
+
+
+print "{ "
+print "'fileName': '"
+print fileName
+print "'"
+print ", 'loc': "
 print loc
+
+print ", 'actualLines': "
+
+print actualCodeCount
+print ", 'commentCount': "
 print commentCount
+
+print ", 'commentDesnity': "
+
+print commentDesnity
+
+print "} "
+
+
