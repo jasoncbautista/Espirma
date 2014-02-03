6454636d47d4b518482f78553cd778a115fc446e
Jason <bjason@gmail.com>
2014-02-02 20:03:48 -0800

    Logging json clean

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var esprima = require('esprima');
 //console.log(JSON.stringify(esprima.parse('var answer = 42'), null, 4));
 // console.log(JSON.stringify(esprima.parse('/*\n * one \n *two  \n  */   \n var two = 1;\n //Another comment \n//Testing one two \n;var answer = 42', {comment: true, loc: true}), null, 4));
 
-
-
 // Read a file:
 var filename = process.argv[2];
 var code = fs.readFileSync(filename);
@@ -12,4 +10,6 @@ var code = fs.readFileSync(filename);
 var jsonResult = esprima.parse(code, {
     comment: true
 });
-console.log(JSON.stringify(jsonResult));
+
+console.log(jsonResult);
+//console.log(JSON.stringify(jsonResult));
