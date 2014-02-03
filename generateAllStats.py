6c4fb54f872fa53991453ed66049d0afb9001902
Jason <bjason@gmail.com>
2014-02-02 21:46:32 -0800

    Printing paths really easy in pytyhon..

---
+++ generateAllStats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+## Very simple python program to get stats for a given fileo
+
+
+import sys
+
+import os
+# TODO: return if sys len <= 1
+
+directoryPath = sys.argv[1]
+
+# TODO make multiple dirs
+
+
+
+
+
+jsFilePathsStr = os.popen("find " + directoryPath + " " + '-name "*.js"').read()
+
+
+
+jsFilePaths = jsFilePathsStr.split("\n")
+
+
+# Here we write a little .js file with our stats
+
+
+print "var commentStats = {"
+for path in jsFilePaths:
+    jsonResult = os.popen("./getCommentsStats  " + path  ).read()
+    print jsonResult
+print "};"
