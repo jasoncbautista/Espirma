725a12ba489eaf81f47a3bb0ce732faf37b4c449
Jason <bjason@gmail.com>
2014-02-02 22:13:44 -0800

    Basic color encoding.

--- lilGrapher.js
+++ lilGrapher.js
@@ -17,17 +17,34 @@ $(document).ready(function(){
         // This is for the scale
         if(noExtra) {
             bar.css({
-                background: black
-
+                background: "black"
+                , width: "2px"
+                , "margin-left": "15px"
             });
             return;
         }
+
+
+        if ( stat.commentDensity < 0.30) {
+            bar.css({
+                background: "yellow"
+            });
+        }
+
+        if ( stat.commentDensity < 0.10) {
+            bar.css({
+                background: "red"
+            });
+        }
+
+
+
     };
 
     var parent = $("#commentPercent");
     addStatsToDom(parent, {
         commentDensity: 1.0
-    });
+    }, true);
     _.each(commentStats, function(stat){
         addStatsToDom(parent, stat);
     });
